f17a124918977702ae11f089ce4e35b27f788d43
hardyjoshua <hardyjoshua160@gmail.com>
2020-11-02 16:16:23 -0500

    add: buefy datepicker

--- src/components/CalendarView.vue
+++ src/components/CalendarView.vue
@@ -1,7 +1,16 @@
 <template>
-  <div class="calendar-view">
-    <h1>Hello World!</h1>
-  </div>
+  <section class="calendar-view">
+    <b-field label="Select a date">
+      <b-datepicker
+        v-model="selected"
+        :show-week-number="showWeekNumber"
+        :locale="locale"
+        placeholder="Click to select..."
+        icon="calendar-today"
+        trap-focus>
+      </b-datepicker>
+    </b-field>
+  </section>
 </template>
 
 <script>
@@ -9,12 +18,43 @@ export default {
   name: 'CalendarView',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      selected: new Date(),
+      showWeekNumber: false,
+      locale: undefined // Browser locale
+    }
   }
 }
 </script>
 
-<style scoped>
-  h1 {
-    text-align: center;
+<style lang="scss">
+  .calendar-view {
+    max-width: 420px;
+    padding: 40px;
+
+    // override styles of datepicker
+    & header {
+      background-color: initial;
+    }
+
+    & .control.has-icons-left .input {
+      padding-left: 1rem;
+    }
+
+    & .pagination-previous,
+    & .pagination-next {
+      margin-top: 0;
+    }
+
+    & .select:not(.is-multiple):not(.is-loading)::after {
+      top: 45%;
+    }
+
+    & .pagination {
+      font-size: 1rem;
+    }
   }
+  
 </style>
