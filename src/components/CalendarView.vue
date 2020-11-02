98dc0f028f0a16f725aa17f9d7fea670e4e30b19
hardyjoshua <hardyjoshua160@gmail.com>
2020-11-02 15:04:46 -0500

    init: calendar component with hello-world

---
+++ src/components/CalendarView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="calendar-view">
+    <h1>Hello World!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CalendarView',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+  h1 {
+    text-align: center;
+  }
+</style>
